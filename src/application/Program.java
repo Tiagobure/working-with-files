625f883114334496277dc740837e1f50a51198b3
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-02-10 13:54:07 -0300

    trabalhando com arquivos

---
+++ src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dados dos produtos: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Price: ");
+		sc.nextLine();
+		Double price = sc.nextDouble();
+		System.out.print("Quantity: ");
+		Integer quantity = sc.nextInt();
+		
+		
+		
+		
+		
+	}
+
+}
